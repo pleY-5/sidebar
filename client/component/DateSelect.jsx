d9e37d8b545a10153cfe25ecb3c4096a1032fabb
EddieLo <eddielo106@gmail.com>
2018-09-11 15:00:07 -0700

    finish date input visuals

--- client/component/DateSelect.jsx
+++ client/component/DateSelect.jsx
@@ -3,9 +3,9 @@ import styles from '../sidebar.css';
 
 const DateSelect = ({ currentDate }) => (
   <div className={styles.dateContainer}>
-    <img src='images/calendar.PNG' className={styles.dateIcon}/>
+    <img src='images/calendar.PNG'/>
     <input type='text' value={currentDate} readOnly className={styles.dateText}></input>
-    <img src='images/downarrow.PNG'/>
+    <img src='images/downarrow.PNG' className={styles.dateArrowIcon}/>
   </div>
 );
 
