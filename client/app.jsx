3bbc1a7922d73663e999c540f9d272beca76797f
EddieLo <eddielo106@gmail.com>
2018-09-14 23:53:21 -0700

    fix unselectable empty box

--- client/app.jsx
+++ client/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import Sidebar from './component/Sidebar';
+import Sidebar from './component/Sidebar.jsx';
 
 ReactDOM.render(
   <Provider store={store}>
--- client/component/CalendarWeek.jsx
+++ client/component/CalendarWeek.jsx
@@ -3,11 +3,14 @@ import styles from '../sidebar.css';
 
 const CalendarWeek = ({ calendarWeek, selectedDate, calendarDate, handleDateClick }) => {
   const selected = new Date(selectedDate);
+  const thisYear = new Date().getFullYear();
   const thisMonth = new Date().getMonth();
   const today = new Date().getDate();
   const selDay = selected.getDate();
   const selMonth = selected.getMonth();
+  const selYear = selected.getFullYear();
   const calMonth = calendarDate.getMonth();
+  const calYear = calendarDate.getFullYear();
 
   return (
     <tr>
@@ -15,14 +18,17 @@ const CalendarWeek = ({ calendarWeek, selectedDate, calendarDate, handleDateClic
         let onClick = () => handleDateClick(day, calendarDate);
         let classes = [styles.calendarDay];
         let text = day;
-        if (thisMonth === calMonth && day === today) { classes.push(styles.today); }
-        if (selMonth === calMonth && day === selDay) { 
+        if (thisMonth === calMonth && day === today && thisYear === calYear) { 
+          classes.push(styles.today); 
+        }
+        if (selMonth === calMonth && day === selDay && calYear === selYear) { 
           classes.push(styles.selectedDate); 
           onClick = () => {};
         } 
-        if ((thisMonth === calMonth && day < today) || 
-            (thisMonth + 1 === calMonth && day > today) || 
-            (thisMonth !== calMonth && thisMonth + 1 !== calMonth)) { 
+        if (((thisMonth === calMonth && day < today) || 
+             (thisMonth + 1 === calMonth && day > today) || 
+             (thisMonth !== calMonth && thisMonth + 1 !== calMonth) || 
+             thisYear !== calYear) && day !== 0) {
           classes.push(styles.invalidDate); 
           onClick = () => {};
         }
