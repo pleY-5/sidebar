1dd232beca102062857897adbf430bb0702441c2
Ruby Lee <rubymleee@gmail.com>
2018-10-11 12:14:23 -0700

    implement loaderio

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-// require('newrelic');
+require('newrelic');
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -7,6 +8,10 @@ const bodyParser = require('body-parser');
 const router = require('./routes.js');
 
 app.use(bodyParser.json());
+
+app.use(process.env.LOADER_URL, (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'loaderio-7e70dcee6351c8c38041763c268f00f9.txt'));
+});
 
 app.use('/:nameOrId', express.static('public'));
 
--- server/routes.js
+++ server/routes.js
@@ -3,8 +3,7 @@ const router = require('express').Router();
 const { controller } = require('./controller.js');
 // const { cache, controller } = require('./controller.js');
 
-router.get('http://ec2-54-215-253-161.us-west-1.compute.amazonaws.com:7878/loaderio-7e70dcee6351c8c38041763c268f00f9/', controller.restaurants.get);
-// router.get('/restaurants/:nameOrId', controller.restaurants.get);
+router.get('/restaurants/:nameOrId', controller.restaurants.get);
 // router.get('/restaurants/:nameOrId', cache, controller.restaurants.get);
 router.post('/restaurants', controller.restaurants.post);
 
